be7a4cdf6f26d06d7fc715e02e7642058e27ae16
Genya <genya@MacBook.fritz.box>
2023-04-12 20:05:32 +0200

    ...

---
+++ src/countM.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+row_names=[]
+prefix='ACT'
+file_name='/Users/genya/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
+#file_name='/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
+for i in range(1,16):
+  df=pd.read_csv(file_name.format(i), usecols=[0])
+  row_names=row_names+df['MOLECULE'].tolist()
+  #print(row_names)
+molecules=[x for x in row_names if x.startswith(prefix)]
+unique_list=list(dict.fromkeys(molecules))
+print(unique_list, len(unique_list))
