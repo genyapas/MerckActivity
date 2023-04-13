21775b59c2b88ed28d6cd4ea59c0b61c9015caf9
Genya <genya@MacBook.fritz.box>
2023-04-13 15:12:38 +0200

    count least molecule Set

--- src/countM.py
+++ src/countM.py
@@ -6,8 +6,10 @@ file_name='/Users/genya/projects/MerckActivity/TrainingSet/ACT{}_competition_tra
 #file_name='/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 for i in range(1,16):
   df=pd.read_csv(file_name.format(i), usecols=[0])
-  row_names=row_names+df['MOLECULE'].tolist()
+  row_names=df['MOLECULE'].tolist()
+  #row_names=row_names+df['MOLECULE'].tolist()
   #print(row_names)
-molecules=[x for x in row_names if x.startswith(prefix)]
-unique_list=list(dict.fromkeys(molecules))
-print(unique_list, len(unique_list))
+  #molecules=[x for x in row_names if x.startswith(prefix)]
+  #unique_list=list(dict.fromkeys(molecules))
+#print(unique_list, len(unique_list))
+  print(len(row_names))
