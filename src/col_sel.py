2c691ce517dacb1c3aab9b3b37269b6b45f83f0d
Genya <genya@MacBook.fritz.box>
2023-04-16 10:22:13 +0200

    x=molecule, y=act, c=descriptor

--- src/col_sel.py
+++ src/col_sel.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 occurence_0=[]
+#u=1000
 file_name='/Users/genya/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 #file_name='/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 for i in range(7,8):
@@ -14,6 +16,11 @@ for i in range(7,8):
     occurence_0.append(col_list.count(0))
     if col_list.count(0)==0:
       fin_col.append(t)
-  print(fin_col, df.columns[fin_col])
+  for l in range(len(fin_col)):
+    #plt.scatter(df[df.columns[0]].head(u), df[df.columns[1]].head(u), c=df[df.columns[fin_col[l]]].head(u))
+    plt.scatter(df[df.columns[0]], df[df.columns[1]], c=df[df.columns[fin_col[l]]])    
+    plt.colorbar()
+    plt.show()
+  #print(fin_col, df.columns[fin_col])
   #col_sel.append(min(occurence_0))
   #print(col_sel, occurence_0.count(0))
