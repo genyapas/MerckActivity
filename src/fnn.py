a2467e76c6b7f7ee82a69fcee2d7886af9566e77
Genya <genya@MacBook.fritz.box>
2023-04-24 21:29:35 +0200

    loss- plot

--- src/fnn.py
+++ src/fnn.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 file_name='/Users/genya/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 #file_name='/Users/genya/projects/MerckActivity/TestSet/ACT{}_competition_test.csv'
 #file_name='/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 u=1000
 n=0
+epoch_list=[]
+loss_list=[]
 
 class Net(nn.Module):
 
@@ -34,18 +37,22 @@ for i in range(7,8):
     #print(input, len(df.columns))
     target=torch.FloatTensor(df.iloc[:,1].values)
     target=target.view(len(df.index), 1)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(10):
-        running_loss = 0.0
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
+    for epoch in range(100):
+        #running_loss = 0.0
         output=net(input)
         loss = criterion(output, target)
-        print(loss.item())
+        loss_list.append(loss.item())
         loss.backward()
         optimizer.zero_grad()
         optimizer.step()
-        running_loss += loss.item()
-        if n % 10 == 9:
-            print(f'[{epoch + 1}, {n + 1:5d}] loss: {running_loss / 10:.3f}')
-            running_loss = 0.0
-    print('Finished Training')
+        epoch_list.append(epoch)
+    plt.scatter(epoch_list, loss_list)
+    plt.show()
+    #x->epoch, y->loss
+        #running_loss += loss.item()
+        #if n % 10 == 9:
+            #print(f'[{epoch + 1}, {n + 1:5d}] loss: {running_loss / 10:.3f}')
+            #running_loss = 0.0
+    #print('Finished Training')
