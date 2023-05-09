11caeef168776c24d848e7a2f7d7fcdab8b7c028
Genya <genya@MacBook.fritz.box>
2023-05-09 13:51:21 +0200

    experimenting

--- src/snn.py
+++ src/snn.py
@@ -9,30 +9,32 @@ from torch.utils.data import DataLoader, TensorDataset
 file_name='/Users/genya/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 #file_name='/Users/genya/projects/MerckActivity/TestSet/ACT{}_competition_test.csv'
 #file_name='/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
-u=10
-n=0
-epoch_list=[]
-n_iter=500
+
+u = 1000
+n = 0
+n_iter = 500
 
 class Net(nn.Module):
 
     def __init__(self, y):
         super(Net, self).__init__()
         #self.fc1 = nn.Linear(y, int(y/2))
-        self.fc1 = nn.Linear(y, 100)
-        self.fc2 = nn.Linear(100, 1)
-        #self.fc3 = nn.Linear(int(y/3), 1)
+        self.fc1 = nn.Linear(y, u)
+        self.fc2 = nn.Linear(u, u)
+        self.fc3 = nn.Linear(u, 1)
         #self.fc3 = nn.Linear(int(y/2), 1)
         #self.fc4 = nn.Linear(400, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        #x = F.relu(self.fc2(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 for i in range(7,8):
+    epoch_list = []
     input_d = []
+
     df = pd.read_csv(file_name.format(i))
     input_d = int(len(df.columns)-2)
     net = Net(y = input_d)
@@ -46,9 +48,11 @@ for i in range(7,8):
     train_data = TensorDataset(x_train, target)
 
     # create data loader
-    train_loader = DataLoader(dataset = train_data, batch_size = 400)
+    train_loader = DataLoader(dataset = train_data, batch_size = 800, shuffle=True)
+    #train_loader = DataLoader(dataset = train_data, batch_size = 900)
     criterion = nn.MSELoss()
-    optimizer = optim.SGD(net.parameters(), lr = 0.01)
+    optimizer = optim.SGD(net.parameters(), lr = 0.03, momentum=0.9, weight_decay=0.001)
+    #optimizer = optim.SGD(net.parameters(), lr = 0.03, momentum=0.9, weight_decay=0.001)
     running_loss_list = []
 
     for epoch in range(n_iter):
@@ -62,7 +66,7 @@ for i in range(7,8):
             optimizer.step()
             running_loss += loss.item()
 
-        if epoch % 100 == 99:
+        if epoch % 10 == 9:
             print(running_loss, epoch)
             epoch_list.append(epoch)
             running_loss_list.append(running_loss)
