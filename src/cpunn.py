bbf9cb29674b0ae658fc6e3f771e4f3e3f222b36
genyapas <--global>
2023-05-19 18:07:18 +0200

    workload split on cpu

---
+++ src/cpunn.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
+import numpy as np
+import multiprocessing
+
+file_name = '/home/ewgeni/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
+u = 5000
+n = 0
+n_iter = 500
+
+class Net(nn.Module):
+    def __init__(self, y):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(y, u)
+        self.fc2 = nn.Linear(u, int(u/2))
+        self.fc3 = nn.Linear(int(u/2), int(u/2))
+        self.fc4 = nn.Linear(int(u/2), 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+def train_model(rank):
+    epoch_list = []
+    input_d = []
+
+    df = pd.read_csv(file_name.format(rank+8))
+    input_d = int(len(df.columns)-2)
+    net = Net(y=input_d)
+
+    target = df.iloc[:, 1]
+    max_value = target.max()
+    target = target.div(max_value)
+    target = target.subtract(target.mean())
+
+    amplitude = target.max() - target.min()
+    target = target.div(amplitude)
+
+    x_train = torch.FloatTensor(df.iloc[:, 2:len(df.columns)].values)
+    x_train = x_train.add(1)
+    x_train = np.log(x_train)
+
+    target = torch.FloatTensor(target.values)
+    train_data = TensorDataset(x_train, target)
+    train_loader = DataLoader(dataset=train_data, batch_size=int(800 / 12), shuffle=True)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.03, momentum=0.9, weight_decay=0.001)
+    running_loss_list = []
+
+    for epoch in range(n_iter):
+        running_loss = 0
+
+        for x_train, target in train_loader:
+            optimizer.zero_grad()
+            output = net(x_train)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        if epoch % 10 == 9:
+            print(running_loss, epoch)
+            epoch_list.append(epoch)
+            running_loss_list.append(running_loss)
+
+    plt.scatter(epoch_list, running_loss_list)
+    plt.show()
+
+if __name__ == '__main__':
+    num_processes = 12
+    processes = []
+
+    for rank in range(num_processes):
+        p = multiprocessing.Process(target=train_model, args=(rank,))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
