749b1e912ea45edfc140b7a86d1ac746aa213f34
genyapas <genya.pas1@icloud.com>
2023-07-18 23:25:45 +0200

    trying to fix error

--- src/mvpnn.py
+++ src/mvpnn.py
@@ -13,7 +13,7 @@ import random2
 file_name = '/home/ewgeni/projects/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 #file_name = '/home/ipasichn/MerckActivity/TrainingSet/ACT{}_competition_training.csv'
 
-n_out, n_hidden, learning_rate, n_iter, batch_size, n_input, dfa = 1, 8000, 0.01, 100, 8000, 11080, []
+n_out, n_hidden, learning_rate, n_iter, batch_size, n_input, dfa = 1, 8000, 0.01, 10, 8000, 11080, []
 n = 0
 t = n_iter
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -51,7 +51,7 @@ for i in range(1, 16):
     df = pd.read_csv(filename, skiprows=skip)
     dfa.append(df)
 
-dfa = pd.concat(dfa)
+dfa = pd.concat(dfa, axis = 0)
 dfa = dfa.fillna(0)
 
 epoch_list = []
@@ -72,7 +72,7 @@ target = target.div(amplitude)
 x_train = torch.FloatTensor(dfa.iloc[:, 2:len(dfa.columns)].values)
 x_train = x_train.add(1)
 x_train = np.log(x_train)
-    
+
 target = target.values.reshape((-1,1))
 target = torch.FloatTensor(target)
 
@@ -108,25 +108,29 @@ for epoch in range(n_iter):
 epoch_lists.append(epoch_list)
 running_loss_lists.append(running_loss_list)
 
+##Test
+
 file_name = '/home/ewgeni/projects/MerckActivity/TestSet/ACT{}_competition_test.csv'
+dfa = []
+output = []
 
-
-for i in range(1, 16):
+for i in range(1, 2):
     filename = file_name.format(i)
-    n = sum(1 for line in open(filename)) - 1
-    s = 1000
-    skip = sorted(random2.sample(range(1,n+1),n-s))
-    df = pd.read_csv(filename, skiprows=skip)
+    df = pd.read_csv(filename)
     dfa.append(df)
 
-dfa = pd.concat(dfa)
+dfa = pd.concat(dfa, axis = 0)
 dfa = dfa.fillna(0)
+x_test = dfa.iloc[123, 1:len(dfa.columns)].values.reshape(-1, 1)
 
-x_test = torch.FloatTensor(dfa.iloc[:, 2:len(dfa.columns)].values)
-x_test = x_test.add(1)
-x_test = np.log(x_test)
+x_test = torch.FloatTensor(x_test)
+x_test = x_test.to(device)
 
-output = net(x_test)
+for x in x_test:
+    act = net(x)
+    output.append(act)
+#x_test = x_test.add(1)
+#x_test = np.log(x_test)
 print(output)
 
 for i in range(len(epoch_lists)):
